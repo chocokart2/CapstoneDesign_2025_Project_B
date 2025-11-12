0c5ca39e3213cda04924b8fd46daa60a39f9f50c
chocokart2 <chocokart@naver.com>
2025-11-13 00:21:32 +0900

    Update FinishZone.cs

--- ProjectFiles/capstone_2025_Project_B_01/Assets/InternalResources/Scripts/FinishZone.cs
+++ ProjectFiles/capstone_2025_Project_B_01/Assets/InternalResources/Scripts/FinishZone.cs
@@ -9,6 +9,7 @@ public class FinishZone : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (PlayerController.IsPlayer(other) == false) return;
+        if (PlayerController.instance.IsHoldingScroll == false) return;
 
         UiManager.instance.ShowWin();
 
